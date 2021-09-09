ed27e461de4590fa68b9a1f401b966547f243dd4
Paulo Henrique de Oliveira <paulo.freire53@gmail.com>
2021-09-09 15:02:25 -0300

    project with create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+       <p>Fazer o menu aqui</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
